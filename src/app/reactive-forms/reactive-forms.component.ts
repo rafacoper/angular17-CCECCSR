eb2ded894e3b51f85067f8aae0c6a3d5f3a5536e
rafacoper <rafaelbarbosa.tecnologia@outlook.com>
2024-01-24 18:02:22 -0300

    Migrating to formbuilder

--- src/app/reactive-forms/reactive-forms.component.ts
+++ src/app/reactive-forms/reactive-forms.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -11,23 +11,43 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 })
 export class ReactiveFormsComponent {
 
-  registrationForm = new FormGroup({
-    email: new FormControl('Rafael'),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    address: new FormGroup({
-      street: new FormControl(''),
-      complement: new FormControl(''),
-      neighborhood: new FormControl(''),
-      cep: new FormControl(''),
+  constructor(private fb: FormBuilder) {}
+
+  registrationForm = this.fb.group({
+    email: ['rafael@gmail.com'],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      street: [''],
+      complement: [''],
+      neighborhood: [''],
+      cep: [''],
     })
   });
 
+  // registrationForm = new FormGroup({
+  //   email: new FormControl('Rafael'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormGroup({
+  //     street: new FormControl(''),
+  //     complement: new FormControl(''),
+  //     neighborhood: new FormControl(''),
+  //     cep: new FormControl(''),
+  //   })
+  // });
+
   loadApiData() {
-    this.registrationForm.patchValue({
+    this.registrationForm.setValue({
       email: 'Rafael',
       password: '12345678',
       confirmPassword: '12345678',
+      address: {
+        street: 'Uruguai',
+        complement: '1401',
+        neighborhood: 'Zion',
+        cep: '30310520',
+      }
     })
   }
 }
